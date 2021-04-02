b79e1beaa598675c99d01cd4b0a45aa73df1cd83
Beatriz Vilalta <beatrizvilaltajimenez@gmail.com>
2021-04-02 19:03:28 -0300

    Refactored track_organizer

--- service/track_organizer.rb
+++ service/track_organizer.rb
@@ -6,49 +6,6 @@ class TracksOrganizer
 
     attr_accessor :lecture_list
 
-
-    def organize_morning_track
-        track_total_duration = 0
-        total_hours = 0
-        morning_track_list = Array.new
-
-        @lecture_list.each do |current_lecture|
-            total_hours += current_lecture.duration
-
-            if total_hours <= 180
-                morning_track_list.push(current_lecture)
-            end
-        end
-
-        morning_track_list.each do |current_lecture|
-            track_total_duration += current_lecture.duration
-        end
-
-        if track_total_duration != 180 
-            @lecture_list << @lecture_list.shift
-            organize_morning_track
-        else
-            @lecture_list = @lecture_list - morning_track_list
-            return morning_track_list
-        end
-    end
-
-    def orgnize_afternoon_track
-        total_hours = 0
-        afternoon_track = Array.new
-                
-        @lecture_list.each do |current_lecture|
-            total_hours += current_lecture.duration
-
-            if total_hours <= 240
-                afternoon_track.push(current_lecture)
-            end
-        end
-        @lecture_list = @lecture_list - afternoon_track
-
-        return afternoon_track
-    end 
-
     def build_conference
         first_day_morning = organize_morning_track
         second_day_morning = organize_morning_track
@@ -58,5 +15,46 @@ class TracksOrganizer
         conference = Conference.new(first_day_morning, first_day_afternoon, second_day_morning, second_day_afternoon)
 
         return conference
+    end
+
+    def organize_morning_track
+        morning_track_list = build_track(180)
+
+        if exceded_morning_limit(morning_track_list)
+            @lecture_list << @lecture_list.shift
+            organize_morning_track
+        else
+            @lecture_list = @lecture_list - morning_track_list
+            return morning_track_list
+        end
+    end
+
+    def orgnize_afternoon_track    
+       afternoon_track_list = build_track(240)
+
+        @lecture_list = @lecture_list - afternoon_track_list
+
+        return afternoon_track_list
+    end 
+
+    def build_track(maximum_duration)
+        track_list = Array.new
+        total_hours = 0
+        @lecture_list.each do |current_lecture|
+            total_hours += current_lecture.duration
+
+            if total_hours <= maximum_duration
+                track_list.push(current_lecture)
+            end
+        end
+        return track_list
+    end
+
+    def exceded_morning_limit(morning_track_list)
+        track_total_duration = 0
+        morning_track_list.each do |current_lecture|
+            track_total_duration += current_lecture.duration
+        end
+        return track_total_duration != 180
     end
 end
