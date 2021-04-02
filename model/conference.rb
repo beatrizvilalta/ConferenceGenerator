ea50541b4fa4e66e39a6b2d3840c0946b4a67519
Beatriz Vilalta <beatrizvilaltajimenez@gmail.com>
2021-04-01 21:13:24 -0300

    Refactored code

--- model/conference.rb
+++ model/conference.rb
@@ -1,10 +1,10 @@
 class Conference
 
-    def initialize()
-        @first_day_morning = Array.new
-        @first_day_afternoon = Array.new
-        @second_day_morning = Array.new
-        @second_day_afternoon = Array.new
+    def initialize(first_day_morning, first_day_afternoon, second_day_morning, second_day_afternoon)
+        @first_day_morning = first_day_morning
+        @first_day_afternoon = first_day_afternoon
+        @second_day_morning = second_day_morning
+        @second_day_afternoon = second_day_afternoon
     end
 
     attr_accessor :first_day_morning, :first_day_afternoon, :second_day_morning, :second_day_afternoon
--- service/track_organizer.rb
+++ service/track_organizer.rb
@@ -1,74 +1,62 @@
 class TracksOrganizer
 
-    def initialize()
-        @remaining_lectures_list = Array.new
+    def initialize(lecture_list)
+        @lecture_list = lecture_list
     end
 
-    attr_reader :remaining_lectures_list
+    attr_accessor :lecture_list
 
-    
-    def organize_first_day_morning_track(lecture_list, first_track_list)
-        total_hours = 0
-        
-        lecture_list.each do |current_lecture|
-            total_hours += current_lecture.duration
-            if total_hours <= 180
-                first_track_list.push(current_lecture)
-            else
-                @remaining_lectures_list.push(current_lecture) 
-            end
-        end 
-    end
-    
-    def organize_single_track(track_of_the_day_list, maximum_duration)
-        total_hours = 0
-                
-        @remaining_lectures_list.each do |current_lecture|
-            total_hours += current_lecture.duration
 
-            if total_hours <= maximum_duration
-                track_of_the_day_list.push(current_lecture)
-            end
-        end
-        @remaining_lectures_list = @remaining_lectures_list - track_of_the_day_list
-    end
-
-    def organize_second_morning_track(second_day_morning)
+    def organize_morning_track
         track_total_duration = 0
         total_hours = 0
+        morning_track_list = Array.new
 
-        @remaining_lectures_list.each do |current_lecture|
+        @lecture_list.each do |current_lecture|
             total_hours += current_lecture.duration
 
             if total_hours <= 180
-                second_day_morning.push(current_lecture)
+                morning_track_list.push(current_lecture)
             end
         end
 
-        second_day_morning.each do |current_lecture|
+        morning_track_list.each do |current_lecture|
             track_total_duration += current_lecture.duration
         end
 
         if track_total_duration != 180 
-            @remaining_lectures_list << @remaining_lectures_list.shift
-            second_day_morning.clear
-            organize_second_morning_track(second_day_morning)
+            @lecture_list << @lecture_list.shift
+            organize_morning_track
         else
-            @remaining_lectures_list = @remaining_lectures_list - second_day_morning
+            @lecture_list = @lecture_list - morning_track_list
+            return morning_track_list
         end
+    end
+
+    def orgnize_afternoon_track
+        total_hours = 0
+        afternoon_track = Array.new
+                
+        @lecture_list.each do |current_lecture|
+            total_hours += current_lecture.duration
+
+            if total_hours <= 240
+                afternoon_track.push(current_lecture)
+            end
+        end
+        @lecture_list = @lecture_list - afternoon_track
+
+        return afternoon_track
     end 
 
-    def organize_conference_tracks(lecture_list, first_day_morning, first_day_afternoon, 
-        second_day_morning, second_day_afternoon,
-        morning_maximum_duration, afternoon_maximum_duration)
-        
-        organize_first_day_morning_track(lecture_list, first_day_morning)
-        organize_second_morning_track(second_day_morning)
-        organize_single_track(first_day_afternoon, afternoon_maximum_duration)
-        organize_single_track(second_day_afternoon, afternoon_maximum_duration)
+    def build_conference
+        first_day_morning = organize_morning_track
+        second_day_morning = organize_morning_track
+        first_day_afternoon = orgnize_afternoon_track
+        second_day_afternoon = orgnize_afternoon_track
 
-    end  
+        conference = Conference.new(first_day_morning, first_day_afternoon, second_day_morning, second_day_afternoon)
+
+        return conference
+    end
 end
-
-
-    
